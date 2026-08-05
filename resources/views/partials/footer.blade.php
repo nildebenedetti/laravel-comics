5ad8263adf3e7ad786aa966360fd97e5c8d3ef8c
“Nilde <nilde.benedetti@gmail.com>
2026-08-05 15:09:22 +0200

    feat: restructure footer layout with additional sections for navigation

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,23 @@
-<footer>
-    <div class="container d-flex justify-space-between">
-        <div class="brand-block">
-        <img class="img-fluid" src="" alt="">
+<footer class="footer-container">
+    <!-- aside navigation -->
+    <section class="aside-navigation">
+
+    </section>
+    <!-- website navigation -->
+    <section class="site-navigation">
+        <div class="container d-flex justify-space-between">
+            <!-- OVERLAY-LOGO -->
+            <div class="brand-block">
+                <img class="img-fluid" src="" alt="">
+            </div>
+            <!-- navlinks -->
+            <div class="nav-block">
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">About this Website</a>
+                    <a href="">Contacts</a>
+                </nav>
+            </div>
         </div>
-        <div class="nav-block">
-            <nav>
-            <a href="">Home</a>
-            <a href="">About this Website</a>
-            <a href="">Contacts</a>
-            </nav>
-        </div>
-    </div>
+    </section>
 </footer>
