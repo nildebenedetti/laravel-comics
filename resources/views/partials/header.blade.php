d9bab00af37fa7d5955e7e7f55b5c8f5c967cd94
“Nilde <nilde.benedetti@gmail.com>
2026-08-05 13:48:34 +0200

    feat: update header layout with responsive navbar and navigation links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,45 @@
 <header>
-    <div class="title">
-        <h1>Ciao sono l'header</h1>
-    </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="navbar container-fluid mx-3 d-flex justify-space-between">
+            <div class="logo">
+                <a class="navbar-brand" href="#">
+                    <img class="navbar-logo" src="/images/dc-logo.png" alt="DC logo">
+                </a>
+            </div>
+            <div class="nav-links">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">characters</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">comics</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">movies</a>
+                    </li>                             
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">collectibles</a>
+                    </li>                             
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">videos</a>
+                    </li>                             
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">fans</a>
+                    </li>                             
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">news</a>
+                    </li>                             
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">shops</a>
+                    </li> 
+                </ul>
+            </div>
+
+            </div>
+        </div>
+    </nav>
 </header>
